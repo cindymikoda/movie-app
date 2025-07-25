3d31231926e0350bab1ca2073b67e9055794f810
cindymikoda <mikodacindy@gmail.com>
2025-07-26 01:40:39 +0800

    Bonus: starting to set up an error message and loading useState for trending movies.

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,16 @@ const API_OPTIONS = {
 };
 
 const App = () => {
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
   const [searchTerm, setSearchTerm] = useState("");
+  //--
+  const [movieList, setMovieList] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [movieList, setMovieList] = useState([]);
+  //--
   const [trendingMovies, setTrendingMovies] = useState([]);
-  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [tredingIsLoading, setTrendnigIsLoading] = useState([]);
+  const [trendingErrorMessage, setTrendingErrorMessage] = useState([]);
 
   useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
 
